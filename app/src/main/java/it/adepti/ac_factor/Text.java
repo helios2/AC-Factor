ecf614ecc41c2e94650f8d2ecc1b578b41f595c8
helios2 <mariglianofrancesco@gmail.com>
2015-04-08 13:58:24 +0200

    Aggiunto il codice precedente di activity nella classe Text

--- app/src/main/java/it/adepti/ac_factor/Text.java
+++ app/src/main/java/it/adepti/ac_factor/Text.java
@@ -1,4 +1,39 @@
 package it.adepti.ac_factor;
 
+import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import it.adepti.ac_factor.ftp.FTPManager;
+
 public class Text extends FragmentTab {
+
+    private FTPManager myManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //        AsyncTask connectTask = new AsyncTask(){
+//            @Override
+//            protected Object doInBackground(Object[] params) {
+//                Log.d("FTPManager", "doInBackground");
+//                myManager = new FTPManager();
+//                myManager.connectWithFTP("ftp.androidprova.altervista.org",
+//                        "androidprova",
+//                        "dukcivosne70",
+//                        FTPManager.PASSIVE_MODE);
+//                myManager.setWorkingDirectory("/080415/");
+//                myManager.downloadFile("Text_080415", "txt");
+//                return null;
+//            }
+//        };
+//
+//        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.HONEYCOMB)
+//            connectTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+//        else
+//            connectTask.execute();
+    }
 }
