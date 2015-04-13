60e28a6bdf80e8a079b50e415c95619c2abe500d
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2015-04-13 13:49:52 +0200

    Modifica momentanea RemoteServer

--- app/src/main/java/it/adepti/ac_factor/utils/RemoteServer.java
+++ app/src/main/java/it/adepti/ac_factor/utils/RemoteServer.java
@@ -9,21 +9,36 @@ import java.net.URL;
 public class RemoteServer {
 
     public static boolean checkFileExistenceOnServer(String url){
+//        try {
+//            HttpURLConnection.setFollowRedirects(false);
+//            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+//            connection.setRequestMethod("HEAD");
+//            if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+//                Log.d("NotificationService", connection.getResponseMessage());
+//                return true;
+//            }else{
+//                Log.d("NotificationService", connection.getResponseMessage());
+//                return false;
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+        int code = 0;
         try {
-            HttpURLConnection.setFollowRedirects(false);
-            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-            connection.setRequestMethod("HEAD");
-            if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
-                Log.d("NotificationService", connection.getResponseMessage());
-                return true;
-            }else{
-                Log.d("NotificationService", connection.getResponseMessage());
-                return false;
-            }
+            URL myUrl = new URL(url);
+            HttpURLConnection huc =  ( HttpURLConnection )  myUrl.openConnection ();
+            huc.setRequestMethod ("GET");  //OR  huc.setRequestMethod ("HEAD");
+            huc.connect () ;
+            code = huc.getResponseCode();
         } catch (IOException e) {
             e.printStackTrace();
-            return false;
         }
+
+        if(code==200)
+            return true;
+        else
+            return false;
     }
 
 }
