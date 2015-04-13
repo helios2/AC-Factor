2949e901e3e34c08fa49733ee132abdcee692025
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2015-04-13 14:04:01 +0200

    Cambiato il metodo checkFileExistence

--- app/src/main/java/it/adepti/ac_factor/utils/RemoteServer.java
+++ app/src/main/java/it/adepti/ac_factor/utils/RemoteServer.java
@@ -1,7 +1,5 @@
 package it.adepti.ac_factor.utils;
 
-import android.util.Log;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,33 +7,18 @@ import java.net.URL;
 public class RemoteServer {
 
     public static boolean checkFileExistenceOnServer(String url){
-//        try {
-//            HttpURLConnection.setFollowRedirects(false);
-//            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-//            connection.setRequestMethod("HEAD");
-//            if(connection.getResponseCode() == HttpURLConnection.HTTP_OK){
-//                Log.d("NotificationService", connection.getResponseMessage());
-//                return true;
-//            }else{
-//                Log.d("NotificationService", connection.getResponseMessage());
-//                return false;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return false;
-//        }
-        int code = 0;
+        int responseCode = 0;
         try {
-            URL myUrl = new URL(url);
-            HttpURLConnection huc =  ( HttpURLConnection )  myUrl.openConnection ();
-            huc.setRequestMethod ("GET");  //OR  huc.setRequestMethod ("HEAD");
-            huc.connect () ;
-            code = huc.getResponseCode();
+            URL checkContentURL = new URL(url);
+            HttpURLConnection connection =  ( HttpURLConnection )  checkContentURL.openConnection ();
+            connection.setRequestMethod("GET");
+            connection.connect() ;
+            responseCode = connection.getResponseCode();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if(code==200)
+        if(responseCode == HttpURLConnection.HTTP_OK)
             return true;
         else
             return false;
