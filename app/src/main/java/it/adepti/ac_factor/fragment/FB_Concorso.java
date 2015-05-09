62d56e1dc9f8e0b91770b9e24c8e25e00fee3a8e
helios2 <mariglianofrancesco@gmail.com>
2015-05-09 12:33:21 +0200

    Android version check on FB_Concorso.java

--- app/src/main/java/it/adepti/ac_factor/fragment/FB_Concorso.java
+++ app/src/main/java/it/adepti/ac_factor/fragment/FB_Concorso.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,12 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
 import it.adepti.ac_factor.R;
+import it.adepti.ac_factor.youtube.DeveloperKey;
 
 public class FB_Concorso extends Fragment {
 
@@ -22,6 +28,11 @@ public class FB_Concorso extends Fragment {
     // WebPage URL
     private String pageURL = "http://androidprova.altervista.org/Concorso/facebook_posts.html";
 
+    // YouTubePlayer Fragment
+    private YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+    private YouTubePlayer yTubePlayer;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,47 +41,71 @@ public class FB_Concorso extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        try {
-            WebView.class.getMethod("onResume").invoke(FB_View);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(Build.VERSION.SDK_INT >= 15){
+            try {
+                WebView.class.getMethod("onResume").invoke(FB_View);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        try {
-            WebView.class.getMethod("onPause").invoke(FB_View);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(Build.VERSION.SDK_INT >= 15) {
+            try {
+                WebView.class.getMethod("onPause").invoke(FB_View);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fb_concorso_layout, container, false);
+        View v;
 
-        FB_View = (WebView) v.findViewById(R.id.fb_webView);
-        FB_View.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+        if(Build.VERSION.SDK_INT >= 15) {
+            v = inflater.inflate(R.layout.fb_concorso_layout, container, false);
+            FB_View = (WebView) v.findViewById(R.id.fb_webView);
+            FB_View.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
+
+            WebSettings webSettings = FB_View.getSettings();
+            FB_View.setWebChromeClient(new WebChromeClient());
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setPluginState(WebSettings.PluginState.ON);
+            webSettings.setAllowFileAccess(true);
+            if (Build.VERSION.SDK_INT >= 21) {
+                webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
             }
-        });
 
-        WebSettings webSettings = FB_View.getSettings();
-        FB_View.setWebChromeClient(new WebChromeClient());
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setPluginState(WebSettings.PluginState.ON);
-        webSettings.setAllowFileAccess(true);
+            FB_View.loadUrl("https://m.youtube.com/watch?list=PLNuxVPAQYaGVGCt_b8ZFiI7G1zPTwFnKe&v=33z2N58HWXQ");
+        } else {
+            v = inflater.inflate(R.layout.fragment_youtube, container, false);
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+            transaction.add(R.id.youtube_fragment, youTubePlayerFragment).commit();
 
-        if(Build.VERSION.SDK_INT >= 21) {
-            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            youTubePlayerFragment.initialize(DeveloperKey.DEVELOPER_KEY, new YouTubePlayer.OnInitializedListener(){
+                @Override
+                public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                    yTubePlayer = youTubePlayer;
+                    yTubePlayer.loadPlaylist("PL55713C70BA91BD6E");
+                    yTubePlayer.play();
+                }
+
+                @Override
+                public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+                }
+            });
         }
-
-        FB_View.loadUrl("https://m.youtube.com/watch?list=PLNuxVPAQYaGVGCt_b8ZFiI7G1zPTwFnKe&v=33z2N58HWXQ");
 
 //        String html = "";
 //        html += "<html><body>";
